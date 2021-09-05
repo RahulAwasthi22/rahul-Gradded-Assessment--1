55c90de2025d2fc5d7dd3db20ee27151825db98d
RahulAwasthi22 <89737435+RahulAwasthi22@users.noreply.github.com>
2021-09-05 22:55:07 +0530

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,21 @@
+package com.rahulawasthi.main;
+import java.util.Scanner;
+
+import com.rahulawasthi.departments.*;
+
+public class Driver {
+	private static final Scanner sc = new Scanner( System.in );
+	public static void main(String[] args) {
+			
+		System.out.println( "Choose your department : " );
+		System.out.println( "1. Super Department" );
+		System.out.println( "2. Admin Department" );
+		System.out.println( "3. Hr Department" );
+		System.out.println( "4. Tech Department" );
+		int choice = sc.nextInt();
+		
+		
+	}
+	
+
+}
---
+++ SuperDepartment.java
@@ -0,0 +1,18 @@
+package com.rahulawasthi.departments;
+
+public class SuperDepartment {
+	
+	
+	 String departmentName;
+	 String getTodaysWork;
+	 String getWorkDeadline;
+	 String isTodayAHoliday;
+	
+	public SuperDepartment( String departmentName, String getTodaysWork, String getWorkDeadline, String TodayAHoliday) {
+		
+		
+		
+		
+		
+	}
+}
